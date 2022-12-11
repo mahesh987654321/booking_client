897b3c34db012a3e9c87b0c12c09cbb31733b9f2
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-12-11 20:49:05 +0600

    footer

--- src/components/maillist/Maillist.jsx
+++ src/components/maillist/Maillist.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import "./maillist.css";
 const Maillist = () => {
   return (
-    <div>Maillist</div>
-  )
-}
+    <div>
+      <div className="mail">
+        <h1 className="mailTitle">Save time, save money!</h1>
+        <span className="mailDesc">
+          Sign up and we'll send the best deals to you
+        </span>
+        <div className="mailInputContainer">
+          <input type="text" placeholder="Your Email" />
+          <button>Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Maillist
+export default Maillist;
